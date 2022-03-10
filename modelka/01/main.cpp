1266fe20fae58999829c8e422c394b68f121c107
SecretDzen <turretbox@gmail.com>
2022-03-10 14:45:13 +0700

    setup

--- modelka/01/main.cpp
+++ modelka/01/main.cpp
@@ -6,10 +6,12 @@ int main() {
   int total_recieve = 0;
   int total_proccess = 0;
   int queue = 0;
+
+  /*
   cout << "| STEP | Int Rec | Int Proc | Tot Rec | Queue | Tot Wait |"
        << "| PC1 Free | PC1 Downtime | PC1 Work End |"
        << "| PC2 Free | PC2 Downtime | PC2 Work End |" << endl;
-
+  */
   for (int i = 0; i < 100; i++) {
     queue++;
 
@@ -29,11 +31,12 @@ int main() {
     } else {
       PC2.T_downtime += T_recieve;
     }
-
+    /*
     printf("|%6d|%9d|%10d|", i, T_recieve, T_proccess);
     printf("%9d|%7d|%10d|", total_recieve, queue, PC1.T_wait + PC2.T_wait);
     printf("|%10d|%14d|%14d|", PC1.is_free, PC1.T_downtime, PC1.T_work_end);
     printf("|%10d|%14d|%14d|\n", PC2.is_free, PC2.T_downtime, PC2.T_work_end);
+    */
   }
 
   int total_wait = PC1.T_wait + PC2.T_wait;
@@ -41,6 +44,7 @@ int main() {
   double P_in_sys = total_proccess / 100.0;
   double P_PC1_downtime = PC1.T_downtime / 100.0;
   double P_PC2_downtime = PC2.T_downtime / 100.0;
+
   cout << "Average waiting time: " <<  P_total_wait << "sec" << endl;
   cout << "Average time of task in sys: " << P_in_sys << "sec" << endl;
   cout << "Probability of downtime PC1: " << P_PC1_downtime << endl;
